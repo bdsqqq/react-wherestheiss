9d795c392e4c3ac6fbac2562bcc7a147ab62c37b
bdsqqq <igorbedesqui@gmail.com>
2020-03-09 17:39:58 -0300

    components implementation and ISS logic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import useInterval from './useInterval'
+import Body from './body'
+import Footer from './footer'
+import IssMap from './issMap'
 import './App.css';
 
 function App() {
+  const nada = () => {}
+  const [data, setData] = useState(null);
+  const [lat, setLat] = useState(0);
+  const [lon, setLon] = useState(0);
+  const [delay, setDelay] = useState(0);
+
+  async function fetchData() {
+    const api_url = 'https://api.wheretheiss.at/v1/satellites/25544'
+
+    const response = await fetch(api_url);
+    const jsonData = await response.json();
+    
+    setData(jsonData);
+    setLat(jsonData.latitude);
+    setLon(jsonData.longitude);
+
+    ((delay == 0) ? setDelay(3000) : nada() )
+    console.log(data, "inner");
+  };
+  console.log(data, "outter")
+
+  useInterval(() => {
+    //fetchData();
+  }, delay);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div>
+      <h1>Where's the ISS at??</h1>
     </div>
+    <Body data={data}/>
+    <IssMap lat={lat} lon={lon}/>
+    <Footer/>
+    </>
   );
 }
 
